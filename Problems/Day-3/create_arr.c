724867237861dd1dbe5697ee810a3428d3532d7f
azim <azim>
2025-04-05 17:43:00 +0530

    create and display arr

---
+++ Problems/Day-3/create_arr.c
@@ -0,0 +1,7 @@
+// Function to read n items into an array 
+
+void create_array(int a[], int n) {
+    for(int i=0;i<n;i++) {
+        scanf("%d",a[i]);
+    }
+}
---
+++ Problems/Day-3/display_arr.c
@@ -0,0 +1,7 @@
+// Function to display n elements of array 
+
+void display_array(int a[], int n) {
+    for (int i=0;i<n;i++) {
+        printf("%d",a[i]);
+    }
+}
