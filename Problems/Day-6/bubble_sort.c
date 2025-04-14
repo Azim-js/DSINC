18e0e99ff5f5dad9ee4bcfd7d77546d6d7f022a4
azim <azim>
2025-04-14 18:43:14 +0530

    Day6
    bubble sort func

---
+++ Problems/Day-6/bubble_sort.c
@@ -0,0 +1,17 @@
+// Example 2.9 Function to sort the array elements in ascending order.
+
+void bubble_sort(int a[], int n){
+    int j;
+    int i;
+    int temp;
+
+    for (j=1;j<n;j++){
+        for (i=0;i<n;i++){
+            if(a[i]>a[i+1]){
+                temp =a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+        }
+    }
+}
